8186493e3d5c03b0cf78df743dbb1375feb6a80e
Sunny Lam <sunny.lam@gmail.com>
2018-03-02 21:23:43 -0500

    revised code skeleton

--- dlXkcd/dlXkcd_030218_1.py
+++ dlXkcd/dlXkcd_030218_1.py
@@ -3,4 +3,19 @@
 #! python3
 # ! /usr/local/Cellar/python3/3.6.1
 
-import requests, os, bs4
+import requests, os, bs4, logging
+logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
+# logging.disable(logging.CRITICAL)
+
+url = 'http://xkcd.com/' # starting url
+
+os.makedirs('xkcd',exist_ok=True) # store comics in ./xkcd
+
+while not url.endswith('#'):
+	# TODO:  download the page
+	# TODO:  find the URL of the comic image
+	# TODO:  download the image
+	# TODO:  save the image to ./xkcd
+	# TODO:  get the Prev button's url
+
+print('Done')
