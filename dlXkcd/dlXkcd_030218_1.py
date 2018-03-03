1fa16b4a6db66e77d50a588d8db61225f9450638
Sunny Lam <sunny.lam@gmail.com>
2018-03-02 21:50:32 -0500

    revised code and notes

--- dlXkcd/dlXkcd_030218_1.py
+++ dlXkcd/dlXkcd_030218_1.py
@@ -12,9 +12,37 @@ url = 'http://xkcd.com/' # starting url
 os.makedirs('xkcd',exist_ok=True) # store comics in ./xkcd
 
 while not url.endswith('#'):
-	# TODO:  download the page
-	# TODO:  find the URL of the comic image
-	# TODO:  download the image
+	# download the page
+	logging.debug('Downloading the page')
+	print('Downloading page %s...' % url)
+	res = requests.get(url)
+	res.raise_for_status()
+	logging.debug('Request status is:  ')
+	logging.debug(res)
+
+	soup = bs4.BeautifulSoup(res.text,"html.parser")
+	logging.debug('The requests object is:  ')
+	logging.debug(soup)
+
+	# find the URL of the comic image
+	logging.debug('Locating comic image links...')
+	comicElem = soup.select('#comic img')
+	logging.debug('The comicElem object is:  ')
+	logging.debug(comicElem)
+
+	if comicElem == []:
+		print("Could not find comic image.")
+	else:
+		comicUrl = comicElem[0].get('src') # find the src attribute and get its value
+		logging.debug('The comicUrl is:  ')
+		logging.debug(comicUrl)
+
+	# download the image
+	logging.debug('Downloading the comic image starts...')
+	print("Downloading image %s..." % (comicUrl))
+	res = requests.get(comicUrl)
+	res.raise_for_status()
+
 	# TODO:  save the image to ./xkcd
 	# TODO:  get the Prev button's url
 
--- docs/scratch.md
+++ docs/scratch.md
@@ -8,3 +8,9 @@ The Prev button has a `rel` HTML attribute with the value `prev`
 
 The first comic's Prev button links to the [http://xkcd.com/#](http://xkcd.com/#) URL, meaning there aren't any more previous pages...
 
+	os.makedirs('xkcd',exist_ok=True) # store comics in ./xkcd
+
+ensures that the folders is real
+
+the `exist_ok=True` stops any exceptions if the folder's already there
+
